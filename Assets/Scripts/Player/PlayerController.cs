125d169fd449dd90daa810428441f1b6cd623fdd
Akashkapoor <150754201+akashkapoordev@users.noreply.github.com>
2025-02-04 11:24:36 -0500

    Adding OnKeyPickedUp Listner in PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,11 +28,15 @@ public class PlayerController
         playerState = PlayerState.InDark;
 
         EventService.Instance.OnLightSwitchToggled.AddListener(onLightSwitch);
+        EventService.Instance.OnKeyPickup.AddListener(OnKeyPicked);
     }
+
+
 
     ~PlayerController()
     {
         EventService.Instance.OnLightSwitchToggled.RemoveListener(onLightSwitch);
+        EventService.Instance.OnKeyPickup.RemoveListener(OnKeyPicked);
     }
     public void Interact() => IsInteracted = Input.GetKeyDown(KeyCode.E) ? true : (Input.GetKeyUp(KeyCode.E) ? false : IsInteracted);
 
@@ -84,5 +89,11 @@ public class PlayerController
             PlayerState = PlayerState.None;
         else
             PlayerState = PlayerState.InDark;
+    }
+
+
+    private void OnKeyPicked(int key)
+    {
+        KeysEquipped = key;
     }
 }
