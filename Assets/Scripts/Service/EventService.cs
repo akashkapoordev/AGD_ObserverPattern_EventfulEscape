cd6378edf224d27b4ee576480e83a441848de9b8
Akashkapoor <150754201+akashkapoordev@users.noreply.github.com>
2025-02-04 09:21:28 -0500

    Create an Event Service

---
+++ Assets/Scripts/Service/EventService.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EventService
+{
+    private static EventService instance;
+
+    public static EventService Instance { 
+        get 
+        {
+            if(instance == null)
+            {
+                instance = new EventService();
+            }
+            return instance;
+        } 
+    }
+
+    public EventController OnLightSwitchToggled;
+
+    public EventService()
+    {
+        OnLightSwitchToggled = new EventController();
+    }
+
+}
